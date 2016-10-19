fd745d7e0951f72f96220d75d0c1756c24592f38
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-19 15:09:59 +0530

    Create NextStep.js

---
+++ Code-yours-own-Adventure/NextStep.js
@@ -0,0 +1,14 @@
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+if (userAnswer == "yes") {
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+} if (userAnswer == "no") {
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
+var feedback = prompt("Rate yours Game out of 10");
+if (feedback > 8) {
+    console.log("Thank you! We should race at the next concert!");
+} if (feedback < 8) {
+    console.log("I'll keep practicing coding and racing.");
+}
+    
