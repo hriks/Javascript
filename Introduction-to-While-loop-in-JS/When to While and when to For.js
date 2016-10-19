7968775eb5deddd8957acd53269b1d9c541b7817
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-19 19:46:48 +0530

    Create When to While and when to For.js

---
+++ Introduction-to-While-loop-in-JS/When to While and when to For.js
@@ -0,0 +1,12 @@
+var count = 1;
+
+while(count < 10) {
+    count++;
+    console.log("This is a for loop and the count is...");
+    console.log("The count is " + count);
+}
+for(count < 10;) {
+   count++;
+   console.log("This is a for loop and the count is...");
+   console.log("The count is " + count);
+}
