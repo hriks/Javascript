b16c2ef6b5afdec0604e7803fae2c26d20dc9275
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-19 17:39:12 +0530

    Create Global vs Local Variable.js

---
+++ Code-yours-own-Adventure/Global vs Local Variable.js
@@ -0,0 +1,12 @@
+var my_number = 7; //this has global scope
+
+var timesTwo = function(number) {
+    var my_number = number * 2;
+    console.log("Inside the function my_number is: ");
+    console.log(my_number);
+}; 
+
+timesTwo(7);
+
+console.log("Outside the function my_number is: ")
+console.log(my_number);
