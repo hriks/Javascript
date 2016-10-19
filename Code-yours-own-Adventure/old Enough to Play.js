434b08367c85b187e0d8338676ad85bc16a54fbe
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-19 14:52:08 +0530

    Create old Enough to Play.js

---
+++ Code-yours-own-Adventure/old Enough to Play.js
@@ -0,0 +1,10 @@
+confirm("am ready to go");
+var age = prompt("What's yours Age??");
+if (age < 13)
+{
+    console.log("You are not Allowed to Play");
+}
+else
+{
+    console.log("Go Ahead and play Dear");
+}
