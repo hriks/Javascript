4574145e9b0f2d2a3a3fd3fcb770e27ce01a4a01
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 14:49:03 +0530

    Create Adding to an existing switch.js

---
+++ More-on-Control-Flow/Adding to an existing switch.js
@@ -0,0 +1,15 @@
+var color = prompt("What's your favorite primary color?","Type your favorite color here");
+
+switch(color) {
+  case 'red':
+    console.log("Red's a good color!");
+    break;
+  case 'blue':
+    console.log("That's my favorite color, too!");
+    break;
+    case 'yellow':
+    console.log("Great better color choosen");
+    break;  
+  default:
+    console.log("I don't think that's a primary color!");
+}
