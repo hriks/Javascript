41be56077f1a5e8ddbdf97cd75e27e64e0d29d0f
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 15:08:29 +0530

    Create And.js

---
+++ More-on-Control-Flow/And.js
@@ -0,0 +1,16 @@
+var hungry = true;
+var foodHere = true;
+// Declare your variables here!
+
+
+var eat = function() {
+    if (hungry === true && foodhere == true)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+  // Add your if/else statement here!
+  
+};
