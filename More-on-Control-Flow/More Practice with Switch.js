2ad494174670bb9268c8be6c624dbee7a22e086d
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 14:55:09 +0530

    Create More Practice with Switch.js

---
+++ More-on-Control-Flow/More Practice with Switch.js
@@ -0,0 +1,15 @@
+var answer = prompt("Add your question here!");
+
+switch(answer) {
+  case 'laptop':
+    console.log("I too had one");
+    break;
+    case 'Mobile':
+    console.log("I dont Have that");
+    break;
+    default:
+    console.log("fuck off");
+    break;
+  // Add your code here!
+  
+}
