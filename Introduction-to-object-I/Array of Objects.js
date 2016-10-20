c96e110672f75e2e8191bc294eb24b9d3715266c
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:34:09 +0530

    Create Array of Objects.js

---
+++ Introduction-to-object-I/Array of Objects.js
@@ -0,0 +1,13 @@
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// Now we can make an array of people
+var family = new Array();
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy", 6);
+// add the last family member, "timmy", who is 6 years old
