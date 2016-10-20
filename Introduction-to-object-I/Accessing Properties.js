bf956da0c133f3adf06a3a838101ac16beff78b3
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:04:53 +0530

    Create Accessing Properties.js

---
+++ Introduction-to-object-I/Accessing Properties.js
@@ -0,0 +1,14 @@
+var bob = {
+  name: "Bob Smith",
+  age: 30
+};
+var susan = {
+  name: "Susan Jordan",
+  age: 25
+};
+// here we save Bob's information
+var name1 = bob.name;
+var age1 = bob.age;
+// finish this code by saving Susan's information
+var name2 = susan.name
+var age2 = susan.age
