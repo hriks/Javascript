1d9c720d4efb4e7e62a084b5c677f3d27faf8d6c
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:19:20 +0530

    Create The This Keyword.js

---
+++ Introduction-to-object-I/The This Keyword.js
@@ -0,0 +1,11 @@
+// here we define our method using "this", before we even introduce bob
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+// and down here we just use the method we already made
+bob.setAge = setAge;
+  
+bob.setAge(50)// change bob's age to 50 here
