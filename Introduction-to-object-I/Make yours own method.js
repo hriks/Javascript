6dc19d4e95644910377b0818ef3e1eff6b8f6bb8
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:20:53 +0530

    Create Make yours own method.js

---
+++ Introduction-to-object-I/Make yours own method.js
@@ -0,0 +1,13 @@
+var rectangle = new Object();
+rectangle.height = 3;
+rectangle.width = 4;
+// here is our method to set the height
+rectangle.setHeight = function (newHeight) {
+  this.height = newHeight;
+};
+// help by finishing this method
+rectangle.setWidth = function (newWidth){
+  this.width = newWidth;
+}
+rectangle.setWidth(8);
+rectangle.setHeight(6);
