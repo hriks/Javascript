ade8ef68f7029d4d7043d8debd104ef588994b97
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 14:06:34 +0530

    Create The While Loop!.js

---
+++ Dragon-Salayer/The While Loop!.js
@@ -0,0 +1,8 @@
+var slaying = true;
+var youHit = Math.floor(Math.random()*2);
+var damageThisRound = Math.floor(Math.random()*5 +1)
+var totalDamage = 0;
+while (slaying === true)
+{
+    slaying = false;
+}
