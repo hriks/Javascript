7abfe377dc94ad20e1d6db3bbf9005ba858804d8
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:39:39 +0530

    Create constumizing constructors.js

---
+++ Introduction-to-object-I/constumizing constructors.js
@@ -0,0 +1,12 @@
+var harry_potter = new Object();
+harry_potter.pages = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book (pages, author) {
+    this.pages = pages;
+    this.author = author;
+}
+
+// Use our new constructor to make the_hobbit in one line
+var the_hobbit = new Book(320,"J.R.R. Tolkien");
