6531cf022560df49edcd63241eade00c707f72aa
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:38:03 +0530

    Rename Properties.js to Properties 31.js

---
+++ Introduction-to-object-I/Properties 31.js
@@ -0,0 +1,9 @@
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species = "beagle";
+// use bracket notation for snoopy's age
+var age = snoopy["age"] = 10;
