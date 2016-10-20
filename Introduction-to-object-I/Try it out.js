579606d7c989d31b99a8a56445aa3b0ca484490e
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:26:30 +0530

    Create Try it out.js

---
+++ Introduction-to-object-I/Try it out.js
@@ -0,0 +1,11 @@
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(age, breed, color) {
+ this.age = age;
+ this.breed = breed;
+ this.color = color;
+}
