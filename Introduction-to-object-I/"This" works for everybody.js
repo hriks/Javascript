808a3a05fc9f5e8bfd1a2566a60ea6e231995b1c
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:20:08 +0530

    Create "This" works for everybody.js

---
+++ Introduction-to-object-I/"This" works for everybody.js
@@ -0,0 +1,15 @@
+var setAge = function (newAge) {
+  this.age = newAge;
+};
+// now we make bob
+var bob = new Object();
+bob.age = 30;
+bob.setAge = setAge;
+
+// make susan here, and first give her an age of 25
+var susan = new Object();
+susan.age = 25;
+susan.setAge = setAge;
+
+// here, update Susan's age to 35 using the method
+susan.setAge(35)
