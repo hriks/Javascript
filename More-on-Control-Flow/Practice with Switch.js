ab67c0c1e19d778ed81902b060e5d7e1ed41fc8d
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 14:50:33 +0530

    Create Practice with Switch.js

---
+++ More-on-Control-Flow/Practice with Switch.js
@@ -0,0 +1,17 @@
+var candy = prompt("What's your favorite candy?","Type your favorite candy here.");
+
+switch(candy) {
+  case 'licorice':
+    console.log("Gross!");
+    break;
+  case 'gum':
+    console.log("I like gum!");
+    break;
+  case 'beets':
+    console.log("...is that even a candy?");
+    break;
+    default:
+    console.log("Check once again");
+      // Add your code here!
+  
+}
