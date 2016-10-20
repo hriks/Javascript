d61b0511cb89ac4ea1be0a541a35fca575320e97
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:44:41 +0530

    Create Dizining People.js

---
+++ Buildng-an-Adress-Book/Dizining People.js
@@ -0,0 +1,11 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-777",
+    email: "bob.jones@example.com"
+};
+
+console.log(bob.firstName);
+console.log(bob.lastName);
+console.log(bob.phoneNumber);
+console.log(bob.email);
