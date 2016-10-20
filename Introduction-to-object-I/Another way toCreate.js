4b1e9df5bc10d73306e861d109a27c5bc4932eb7
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:09:17 +0530

    Create Another way toCreate.js

---
+++ Introduction-to-object-I/Another way toCreate.js
@@ -0,0 +1,12 @@
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+
+var susan1 = {
+  name: "Susan Jordan",
+  age: 24
+};
+
+var susan2 = new Object();
+susan2.name = "Susan Jordan";
+susan2.age = 24
