7bccb21a2d81923e2ecda0f8980420f591a420f7
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:17:27 +0530

    Create So what's a method.js

---
+++ Introduction-to-object-I/So what's a method.js
@@ -0,0 +1,11 @@
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// this time we have added a method, setAge
+bob.setAge = function (newAge){
+  bob.age = newAge;
+};
+// here we set bob's age to 40
+bob.setAge(40);
+// bob's feeling old.  Use our method to set bob's age to 20
+bob.setAge(20);
