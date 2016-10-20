d7259c86a6f3ca465486de88e875807fa5ca3ca1
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 16:17:08 +0530

    Create Nouns and Verbs together.js

---
+++ Array-and-Objetcs-in-JS/Nouns and Verbs together.js
@@ -0,0 +1,8 @@
+var phonebookEntry = {};
+phonebookEntry.name = 'Oxnard Montalvo';
+phonebookEntry.number = '(555) 555-5555';
+phonebookEntry.phone = function() {
+  console.log('Calling ' + this.name + ' at ' + this.number + '...');
+};
+
+phonebookEntry.phone();
