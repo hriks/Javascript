dc3050a4976f8f3a1f1b5cae7fbc154bcad0976e
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 15:02:35 +0530

    Create All on your own.js

---
+++ More-on-Control-Flow/All on your own.js
@@ -0,0 +1,15 @@
+var saara = prompt("Hello");
+switch(sdhvd) {
+    case 'a':
+        console.log("A");
+        break;
+        case 'b':
+        console.log("B");
+        break;
+        case 'c':
+        console.log("C");
+        break;
+        default:
+        console.log();
+        break;// Write your code below!
+}
