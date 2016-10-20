2eddd5a2434cded3c7e5b7385bf4cb334ae4b19c
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:23:29 +0530

    Create Coustom Constructions.js

---
+++ Introduction-to-object-I/Coustom Constructions.js
@@ -0,0 +1,9 @@
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+var george = new Person("George Washington", 275);
