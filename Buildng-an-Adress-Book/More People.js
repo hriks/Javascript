8bcbcc1c1e1cbdd2dea46a0238a97aa8d7a1d405
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-20 18:46:28 +0530

    Create More People.js

---
+++ Buildng-an-Adress-Book/More People.js
@@ -0,0 +1,14 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber:"(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+var contacts = [bob,mary];
+console.log(contacts[1].phoneNumber);
