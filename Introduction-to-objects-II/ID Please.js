3c2e5c8dc77a4e7af5cd15ed7011255741dd73fb
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 13:50:22 +0530

    Create ID Please.js

---
+++ Introduction-to-objects-II/ID Please.js
@@ -0,0 +1,7 @@
+var anObj = { job: "I'm an object!" };
+var aNumber = 42;
+var aString = "I'm a string!";
+
+console.log(typeof anObj); // should print "object"
+console.log(typeof aNumber); // should print "number"
+console.log(typeof aString); // should print "string"
