ef6700d331ebbab3c14803b49bccfec358f92689
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 13:49:48 +0530

    Create Who's in yours bracket.js

---
+++ Introduction-to-objects-II/Who's in yours bracket.js
@@ -0,0 +1,10 @@
+var james = {
+    job: "programmer",
+    married: false
+};
+
+// set to the first property name of "james"
+var aProperty = ["job"];
+// print the value of the first property of "james" 
+// using the variable "aProperty"
+console.log(james[aProperty]);
