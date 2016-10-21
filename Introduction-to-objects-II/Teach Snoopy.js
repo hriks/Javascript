eb2c5e01856bcf07bdf731c2c497276f4e8ec6b9
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 14:11:41 +0530

    Create Teach Snoopy.js

---
+++ Introduction-to-objects-II/Teach Snoopy.js
@@ -0,0 +1,17 @@
+function Dog (breed) {
+  this.breed = breed;
+}
+
+// here we make buddy and teach him how to bark
+var buddy = new Dog("Golden Retriever");
+buddy.bark = function() {
+  console.log("Woof");
+};
+buddy.bark();
+
+// here we make snoopy
+var snoopy = new Dog("Beagle");
+snoopy.bark = function() {
+  console.log("Hello");
+};
+snoopy.bark();
