ab093f08711cf6de669c9a5412a9d640c38595c9
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 13:47:26 +0530

    Create An Objective review.js

---
+++ Introduction-to-objects-II/An Objective review.js
@@ -0,0 +1,9 @@
+var james = {
+    job:"programmer",
+    married:false
+};
+function Person(job, married) {
+    this.job = job;
+    this.married = married;
+}
+var gabby = new Person("student", true);
