8fe489f90e979aa1065ae18fe445d4131a8011e8
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 14:12:39 +0530

    Create Prototypes to Rescue.js

---
+++ Introduction-to-objects-II/Prototypes to Rescue.js
@@ -0,0 +1,15 @@
+function Dog (breed) {
+  this.breed = breed;
+};
+
+// here we make buddy and teach him how to bark
+var buddy = new Dog("golden Retriever");
+Dog.prototype.bark = function() {
+  console.log("Woof");
+};
+buddy.bark();
+
+// here we make snoopy
+var snoopy = new Dog("Beagle");
+/// this time it works!
+snoopy.bark();
