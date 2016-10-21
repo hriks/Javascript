c43ac08d0cebf05944b141d8a5ae8b318dc8f8ea
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 14:16:44 +0530

    Create Dry Penguiens.js

---
+++ Introduction-to-objects-II/Dry Penguiens.js
@@ -0,0 +1,17 @@
+// the original Animal class and sayName method
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+// define a Penguin class
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+Penguin.prototype = new Animal();
+// set its prototype to be a new instance of Animal
+
