3a4be950a1a62042f2e225e52305014de6eb1fa2
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 13:48:43 +0530

    Create Literally Speaking.js

---
+++ Introduction-to-objects-II/Literally Speaking.js
@@ -0,0 +1,10 @@
+var james = {
+    job: "programmer",
+    married: false,
+    speak: function() {
+        console.Log("Hello, I am feeling great");
+        console.Log("Hello, I am feeling okay");
+    }
+};
+james.speak("great");
+james.speak("just okay");
