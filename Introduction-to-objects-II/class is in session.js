edba3a1059935a34142d0a144540e9417b98675b
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 14:11:09 +0530

    Create class is in session.js

---
+++ Introduction-to-objects-II/class is in session.js
@@ -0,0 +1,13 @@
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 35);
+
+// make your own class here
+function Circle(radius) {
+    this.radius = radius;
+}
