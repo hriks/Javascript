ca07514d902130986d284777d953bef7870ff990
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 14:12:10 +0530

    Create How do class help us.js

---
+++ Introduction-to-objects-II/How do class help us.js
@@ -0,0 +1,14 @@
+unction Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+// a function that prints the name of any given person
+var printPersonName = function (p) {
+  console.log(p.name);
+};
+
+var bob = new Person("Bob Smith", 30);
+printPersonName(bob);
+
+var me = new Person("John Gaidimas", 150);
+printPersonName(me);
