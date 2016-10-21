e5ebcd937ae6346e223d6931a0d045b2bc9cbffd
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-21 14:13:12 +0530

    Create Prototype Practice.js

---
+++ Introduction-to-objects-II/Prototype Practice.js
@@ -0,0 +1,13 @@
+function Cat(name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
+
+// let's make some cats!
+var cheshire = new Cat("Cheshire Cat", "British Shorthair");
+var gary = new Cat("Gary", "Domestic Shorthair");
+
+Cat.prototype.meow = function() {
+    console.log("Meow!");
+}
+gary.meow()
